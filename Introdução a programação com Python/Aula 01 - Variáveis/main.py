569fdb4e361939fe7d2fdf22007b43469eeab33c
Alysson <alysson.oliveira@ee.ufcg.edu.br>
2022-01-20 18:40:55 -0300

    add primeiras arquivos do curso Python

---
+++ Introdução a programação com Python/Aula 01 - Variáveis/main.py
@@ -0,0 +1,12 @@
+a = int(input('Digite o primeiro valor: '))
+b = int(input('Digite o segundo valor: '))
+
+soma = a + b
+subtracao = a - b
+divisao =  a / b
+multiplica = a * b
+
+print('soma: ' + str(soma))
+print(f'A subtração é {subtracao}')
+print(divisao)
+print(f'A multiplicação é {multiplica}')
