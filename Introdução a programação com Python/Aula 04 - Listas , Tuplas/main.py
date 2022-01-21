ce916aa5e90bb8c7b8f0911080e8ade542e766b0
Alysson <alysson.oliveira@ee.ufcg.edu.br>
2022-01-21 14:21:19 -0300

    aula 04 Listas e tuplas

---
+++ Introdução a programação com Python/Aula 04 - Listas , Tuplas/main.py
@@ -0,0 +1,36 @@
+
+# Listas - desejável sempre ser do mesmo tipo
+lista = [1, 3, 5, 7]
+lista_animal = ['cachorro', 'gato', 'elefante']
+# print(lista)
+# print(lista_animal[1]) # mostra o elemento do array de indice 1
+
+
+# soma  = 0
+# for x in lista:
+#     soma += x
+#     print(soma)
+
+# if 'gato' in lista_animal:
+#     print('Existe gato na lista')
+# else:
+#     print(' não existe o animal ')
+
+# if 'lobo' in lista_animal:
+#     print('Existe gato na lista')
+# else:
+#     print(' não existe o animal ')
+#     lista_animal.append('lobo')
+#     print(lista_animal)
+
+# remove(), pop(), sort()
+
+'''Tuplas - Imutáveis '''
+
+tupla = (1, 10, 12, 14)
+print(tupla)
+
+print(len(tupla))
+
+tupla_animal = tuple(lista_animal)
+print(tupla_animal)
