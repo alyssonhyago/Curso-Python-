65d7355bca4e03cc34072a94ca994866868f73d0
Alysson <alysson.oliveira@ee.ufcg.edu.br>
2022-01-21 17:34:10 -0300

    utilizando datatime

---
+++ Introdução a programação com Python/Aula 09 - Datetime/main.py
@@ -0,0 +1,16 @@
+from datetime import date, time
+
+# data_atual = date.today()
+# print(data_atual.strftime('%d/%m/%Y'))
+# print(data_atual.strftime('%A %B %Y'))
+
+
+def trabalhando_com_time():
+    horario = time(hour =15, minute = 18, second =30)
+    print(horario)
+
+if __name__ == '__main__':
+    trabalhando_com_time()
+
+
+
